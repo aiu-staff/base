c4ef5e6d193420efcfa291fda31aaa18f03155ad
Pavel Khimyack <paul.khimyack@gmail.com>
2024-12-04 13:31:00 +0500

    initial import

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiu-staff/base",  # Название вашего модуля
+    version="0.1.1",  # Версия
+    packages=find_packages(),  # Автоматически находит все пакеты
+    install_requires=[  # Зависимости, если они есть
+
+    ],
+    url="https://github.com/aiu-staff/base.git",  # URL вашего репозитория
+    author="pauchai",
+    author_email="paul.khimyack@gmail.com",
+    description="file downloader ",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
